6c1461c741cb3f5bb4ede888c5eefbe2ec754cf3
Kevin Roos <kevin.roos@web.de>
2014-07-24 16:56:26 +0200

    Initial setup

---
+++ SwitchProxy/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SwitchProxy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewProxy = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProxy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewProxy
+            // 
+            this.dataGridViewProxy.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProxy.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewProxy.Name = "dataGridViewProxy";
+            this.dataGridViewProxy.Size = new System.Drawing.Size(371, 151);
+            this.dataGridViewProxy.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 172);
+            this.Controls.Add(this.dataGridViewProxy);
+            this.Name = "Form1";
+            this.Text = "SwitchProxy";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProxy)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewProxy;
+    }
+}
+
---
+++ SwitchProxy/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwitchProxy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DataTable proxyTable = createProxyTable();
+            dataGridViewProxy.DataSource = proxyTable;
+        }
+
+
+        private DataTable createProxyTable()
+        {
+            DataTable proxyTable = new DataTable();
+            proxyTable.Columns.Add("Active", typeof(bool));
+            proxyTable.Columns.Add("Name", typeof(string));
+            proxyTable.Columns.Add("Address", typeof(string));
+            proxyTable.Columns.Add("Port", typeof(string));
+            proxyTable.Columns.Add("Ignore local settings", typeof(bool));
+            return proxyTable;
+        }
+
+    }
+}
